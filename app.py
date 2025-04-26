0eda40f12ec508f46a613c4b03793bb17666e42f
Javier Vidal Molina <javidalin@gmail.com>
2025-04-26 18:20:08 +0200

    .

--- app.py
+++ app.py
@@ -89,9 +89,9 @@ if st.button("Predecir salario estimado"):
     with st.container():
         st.subheader("Resultado de la estimación")
         st.success(f"""
-        El salario anual estimado es: **{prediccion:,.2f} $**.
+        El salario anual estimado es: {prediccion:,.2f} $.
         Ten en cuenta que esta es una predicción aproximada basada en tu perfil,
-        y podría variar entre **{margen_inferior:,.2f} $** y **{margen_superior:,.2f} $**.
+        y podría variar entre {margen_inferior:,.2f} $** y **{margen_superior:,.2f} $.
         """)
 
         st.caption("Esta estimación puede verse afectada por factores externos como localización exacta, políticas de empresa, demanda del mercado o experiencia práctica.")
